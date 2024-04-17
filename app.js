e4c90e0407c7b0ae2079f199b994f4a3af262ef3
Nawaf Alshalan <noufi02@icloud.com>
2024-04-17 12:07:07 -0400

    Updated Promises

--- app.js
+++ app.js
@@ -15,7 +15,23 @@ function getList() {
   });
 }
 
+const errPar = document.getElementById("error");
+const unorderedList = document.getElementById("list");
+
 // TODO: Handle the resolved or rejected states of the promise
+getList()
+.then((resVal) => {
+  console.log(resVal);
+  resVal.forEach((hobbit) => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    unorderedList.appendChild(li);
+  });
+})
+.catch((rejVal) => {
+  console.error(rejVal);
+  errPar.textContent = rejVal.message;
+})
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
